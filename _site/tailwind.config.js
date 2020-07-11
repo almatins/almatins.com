50d7b77f72dda0a2c27fd7269198c87627b69734
Almatin Siswanto <almatin.siswanto@gmail.com>
2020-07-11 08:16:17 +0700

    config tailwind
    add tailwind and purge css

---
+++ _site/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [
+    './_authors/**/*.md',
+    './_authors/**/*.html',
+    './_includes/**/*.md',
+    './_includes/**/*.html',
+    './_layouts/**/*.md',
+    './_layouts/**/*.html',
+    './_posts/**/*.md',
+    './_posts/**/*.html',
+    './index.html',
+    './blog.html',
+    './search.html',
+    './staff.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
